3c9b8bd4ef1010dcc610e67ee73718ec341809f1
Anthony <anthonyg2803@protonmail.com>
2022-12-13 08:11:52 -0800

    hide ellipsis menu for guests

--- src/components/Media/MediaPost.js
+++ src/components/Media/MediaPost.js
@@ -226,7 +226,7 @@ function MediaPost(props) {
 				className="postUserInfo"
 				style={{ boxShadow: image_url === "" ? "none" : null }}
 			>
-				{((_user && _user.user_id === user_id) || _users[user_id]) && (
+				{_user && (_user.user_id === user_id || _users[user_id]) && (
 					<button className="pActions" onClick={props.setFocusPost}>
 						<img src="/ellipsis.svg" width={20} alt="menu" />
 					</button>
