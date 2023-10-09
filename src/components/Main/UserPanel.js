abe0a8c53e777a562c000ca494419255c0ea1a5c
Anthony <anthonyg2803@protonmail.com>
2023-10-09 15:06:45 -0700

    add icons to user navigation bar

--- src/components/Main/UserPanel.js
+++ src/components/Main/UserPanel.js
@@ -1,33 +1,30 @@
-import { useContext, useRef, useState, useEffect } from 'react';
-import { logout, useAuth } from './firebase'
-import { useNavigate } from 'react-router';
-import { MembersContext, UserContext } from './Contexts';
-function UserPanel (props)
-{
-    const navigate = useNavigate();
-    const {_user, _setUser} = useContext(UserContext);
-    const {_users, _setUsers} = useContext(MembersContext);
-    const [pfp, setPfp] = useState("");
-    const [user_id, setUserID] = useState("");
-    const [show, setShow] = useState(false);
-    useEffect(() => {
-        if(_user !== undefined)
-        {
-            setPfp(_user["pfp"]);
-            setUserID(_user["user_id"]);
-        }
-        else setPfp(null);
-    }, [_user]);
-    const selfRef = useRef();
-    async function logout_user() {
-			/*if(_users[_user.user_id] === undefined)
+import { useContext, useRef, useState, useEffect } from "react";
+import { logout, useAuth } from "./firebase";
+import { useNavigate } from "react-router";
+import { MembersContext, UserContext } from "./Contexts";
+function UserPanel(props) {
+	const navigate = useNavigate();
+	const { _user, _setUser } = useContext(UserContext);
+	const { _users, _setUsers } = useContext(MembersContext);
+	const [pfp, setPfp] = useState("");
+	const [user_id, setUserID] = useState("");
+	const [show, setShow] = useState(false);
+	useEffect(() => {
+		if (_user !== undefined) {
+			setPfp(_user["pfp"]);
+			setUserID(_user["user_id"]);
+		} else setPfp(null);
+	}, [_user]);
+	const selfRef = useRef();
+	async function logout_user() {
+		/*if(_users[_user.user_id] === undefined)
         {
             // add the current user's data to the _users state
             // we make sure to save all of their edits,
             // so it will remain up to date
             _setUsers({..._users, _user});
         }*/
-			/*var _localUsers = localStorage.getItem("users");
+		/*var _localUsers = localStorage.getItem("users");
         if(_localUsers)
         {
             var count = Object.entries(JSON.parse(_localUsers)).length;
@@ -37,62 +34,62 @@ function UserPanel (props)
             // to determine when the client should refresh the cache with new data.
             // in case users have made changes to their profiles since the last snapshot
         }*/
-			setShow(false);
-			await logout();
-			_setUser(undefined); // navigate("/");
-		}
-		useEffect(() => {
-			if (show) {
-				selfRef.current.style.zIndex = null;
-				selfRef.current.style.opacity = "1";
-			} else selfRef.current.style.opacity = "0";
-		}, [show]);
-		function hide() {
-			if (!show) selfRef.current.style.zIndex = "-5";
-		}
-		return (
-			<div
-				ref={selfRef}
-				className="welcome"
-				id="welcome"
-				style={{ zIndex: "-5", opacity: "0", marginRight: "10px" }}
-				onTransitionEnd={hide}
-			>
-				<img
-					className="pfp"
-					style={{ objectFit: "cover", cursor: "pointer" }}
-					src={pfp}
-					onLoad={(e) => setShow(true)}
-					onClick={(e) => navigate("/profile/" + user_id)}
-					alt="profile pic"
-				/>
-				<div className="userOptions">
-					<p>▼</p>
-					<div className="mpContent">
-						{/**verified === false ?
+		setShow(false);
+		await logout();
+		_setUser(undefined); // navigate("/");
+	}
+	useEffect(() => {
+		if (show) {
+			selfRef.current.style.zIndex = null;
+			selfRef.current.style.opacity = "1";
+		} else selfRef.current.style.opacity = "0";
+	}, [show]);
+	function hide() {
+		if (!show) selfRef.current.style.zIndex = "-5";
+	}
+	return (
+		<div
+			ref={selfRef}
+			className="welcome"
+			id="welcome"
+			style={{ zIndex: "-5", opacity: "0", marginRight: "10px" }}
+			onTransitionEnd={hide}
+		>
+			<img
+				className="pfp"
+				style={{ objectFit: "cover", cursor: "pointer" }}
+				src={pfp}
+				onLoad={(e) => setShow(true)}
+				onClick={(e) => navigate("/profile/" + user_id)}
+				alt="profile pic"
+			/>
+			<div className="userOptions">
+				<p>▼</p>
+				<div className="mpContent">
+					{/**verified === false ?
                         <button className="verifyEmail"><span>📧</span>Verify</button>
                     : null */}
-						<button className="logout stealthBtn" onClick={logout_user}>
-							log out
-						</button>
-						<button className="stealthBtn" onClick={() => navigate("/#faq")}>
-							FAQ
-						</button>
-					</div>
+					<button className="logout stealthBtn" onClick={logout_user}>
+						<i class="fas fa-sign-out-alt"></i> log out
+					</button>
+					<button className="stealthBtn" onClick={() => navigate("/#faq")}>
+						<i class="fas fa-question"></i> FAQ
+					</button>
 				</div>
-				{props.notifCount > 0 && (
-					<div className="notifMain" onClick={props.notifEvent}>
-						<p>🔔</p>
-						<span
-							style={{
-								backgroundColor: props.notifCount === 0 ? "#8f8f8f" : "#E74C3C",
-							}}
-						>
-							{props.notifCount}
-						</span>
-					</div>
-				)}
 			</div>
-		);
+			{props.notifCount > 0 && (
+				<div className="notifMain" onClick={props.notifEvent}>
+					<p>🔔</p>
+					<span
+						style={{
+							backgroundColor: props.notifCount === 0 ? "#8f8f8f" : "#E74C3C",
+						}}
+					>
+						{props.notifCount}
+					</span>
+				</div>
+			)}
+		</div>
+	);
 }
 export default UserPanel;
